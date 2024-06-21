91d8c992a765b50326c4f91dcf4520432bd8a475
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-21 15:29:27 +0200

    Creazione classe con variabili

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Movie
+{
+    public $name;
+    public $time;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>php-oop-1</title>
+</head>
+
+<body>
+    <h1>php-oop-1</h1>
+
+</body>
+
+</html>
